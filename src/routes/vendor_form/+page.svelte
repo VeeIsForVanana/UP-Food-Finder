37adbba01458c2b4cde1e55e6a596c928891536e
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-02-19 21:31:06 +0800

    feat: implement vendor_form

---
+++ src/routes/vendor_form/+page.svelte
@@ -0,0 +1,31 @@
+<h1>Vendor Account Creation Form</h1>
+
+<div class="input_div">
+    <label for="username">Username</label>
+    <input id="username" type="text" required/>
+</div>
+
+<div class="input_div">
+    <label for="password">Password</label>
+    <input id="password" type="password" required/>
+</div>
+
+<div class="input_div">
+    <label for="phone_no">Phone Number</label>
+    <input id="phone_no" type="text" required/>
+</div>
+
+<div class="input_div">
+    <label for="security_q">Security Question</label>
+    <input id="security_q" type="text" required/>
+</div>
+
+<style>
+    label {
+        display: block;
+    }
+
+    .input_div {
+        margin: 10px;
+    }
+</style>
