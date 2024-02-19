e232b441f73a301c64680672066f5c66fbe5c3d3
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-02-19 21:11:54 +0800

    feat: change initial message

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to UP Food Finder</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation for the SvelteKit this app is built with.</p>
