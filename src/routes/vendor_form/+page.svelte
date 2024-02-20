361cbba34df7bef1dd751ee829743f86afd00c5f
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-02-20 20:22:20 +0800

    feat: wrap input tags in form tag

--- src/routes/vendor_form/+page.svelte
+++ src/routes/vendor_form/+page.svelte
@@ -1,24 +1,34 @@
+<script>
+    
+</script>
+
 <h1>Vendor Account Creation Form</h1>
 
-<div class="input_div">
-    <label for="username">Username</label>
-    <input id="username" type="text" required/>
-</div>
+<form method="post">
+    <div class="input_div">
+        <label for="username" title="Enter your username. Will not necessarily be displayed publicly.">Username</label>
+        <input id="username" type="text" required/>
+    </div>
 
-<div class="input_div">
-    <label for="password">Password</label>
-    <input id="password" type="password" required/>
-</div>
+    <div class="input_div">
+        <label for="password">Password</label>
+        <input id="password" type="password" required/>
+    </div>
 
-<div class="input_div">
-    <label for="phone_no">Phone Number</label>
-    <input id="phone_no" type="text" required/>
-</div>
+    <div class="input_div">
+        <label for="phone_no">Phone Number</label>
+        <input id="phone_no" type="text" required/>
+    </div>
 
-<div class="input_div">
-    <label for="security_q">Security Question</label>
-    <input id="security_q" type="text" required/>
-</div>
+    <div class="input_div">
+        <label for="security_q">Security Question</label>
+        <input id="security_q" type="text" required/>
+    </div>
+
+    <div class="input_div">
+        <input id="submission" type="submit"/>
+    </div>
+</form>
 
 <style>
     label {
