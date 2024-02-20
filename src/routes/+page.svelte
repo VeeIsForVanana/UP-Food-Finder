a03f6a763fa4d86f8aad3f527501f7a1247b679f
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-02-20 21:16:47 +0800

    feat: add link to vendor_form in index page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
 <h1>Welcome to UP Food Finder</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation for the SvelteKit this app is built with.</p>
+
+<a href="/vendor_form">Sprint 1: Vendor Form</a>
