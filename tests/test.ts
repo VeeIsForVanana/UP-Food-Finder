a19e6970403abb53a54a7d241e9d270a5c729b3a
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-02-27 22:18:38 +0800

    refactor: group tests related to password length check

--- tests/test.ts
+++ tests/test.ts
@@ -26,22 +26,24 @@ test('vendor page submission with filled fields results in congratulatory messag
 	await expect(page.getByRole('heading', { name: "Congratulations, you are now a vendor." })).toBeVisible();
 });
 
-test('vendor page submission with password length less than 8 characters results in error message', async ({ page }) => {
-	await page.goto('/vendor_form');
-	await page.locator("[name=username]").fill("username");
-	await page.locator("[name=password]").fill("pass");
-	await page.locator("[name=phone_no]").fill("0945");
-	await page.locator("[name=security_a]").fill("security answer");
-	await page.locator("[type=submit]").click();
-	await expect(page.getByText("Password must be at within 8 to 32 characters long.")).toBeVisible();
-});
+test.describe('vendor page submission with defective password results in error message', () => {
+	test('vendor page submission with password length less than 8 characters results in error message', async ({ page }) => {
+		await page.goto('/vendor_form');
+		await page.locator("[name=username]").fill("username");
+		await page.locator("[name=password]").fill("pass");
+		await page.locator("[name=phone_no]").fill("0945");
+		await page.locator("[name=security_a]").fill("security answer");
+		await page.locator("[type=submit]").click();
+		await expect(page.getByText("Password must be at within 8 to 32 characters long.")).toBeVisible();
+	});
 
-test('vendor page submission with password length more than 32 characters results in error message', async ({ page }) => {
-	await page.goto('/vendor_form');
-	await page.locator("[name=username]").fill("username");
-	await page.locator("[name=password]").fill("pass");
-	await page.locator("[name=phone_no]").fill("1234567890123456789012345678901234567890");
-	await page.locator("[name=security_a]").fill("security answer");
-	await page.locator("[type=submit]").click();
-	await expect(page.getByText("Password must be at within 8 to 32 characters long.")).toBeVisible();
+	test('vendor page submission with password length more than 32 characters results in error message', async ({ page }) => {
+		await page.goto('/vendor_form');
+		await page.locator("[name=username]").fill("username");
+		await page.locator("[name=password]").fill("pass");
+		await page.locator("[name=phone_no]").fill("1234567890123456789012345678901234567890");
+		await page.locator("[name=security_a]").fill("security answer");
+		await page.locator("[type=submit]").click();
+		await expect(page.getByText("Password must be at within 8 to 32 characters long.")).toBeVisible();
+	});
 });
