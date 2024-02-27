113970df5735bd2a89ded195f9464a4794d96f8a
Immanuel Balete <baletejosiah@gmail.com>
2024-02-27 21:46:59 +0800

    feat(test.ts): edit phone format checking
    Look for error message instead of checking for congratulatory message. Also corrected phone_number typos.

--- tests/test.ts
+++ tests/test.ts
@@ -20,40 +20,40 @@ test('vendor page submission with correctly filled fields results in congratulat
 	await page.goto('/vendor_form');
 	await page.locator("[name=username]").fill("username");
 	await page.locator("[name=password]").fill("password");
-	await page.locator("[name=phone_no]").fill("09123456789");
+	await page.locator("[name=phone_number]").fill("09123456789");
 	await page.locator("[name=security_a]").fill("security answer");
 	await page.locator("[type=submit]").click();
 	await expect(page.getByRole('heading', { name: "Congratulations, you are now a vendor." })).toBeVisible();
 });
 
-test.describe('vendor page submission with incorrect phone number format results in no greeting message', () => {
+test.describe('vendor page submission with incorrect phone number format results in error message', () => {
 	test('too few digits', async ({ page }) => {
 		await page.goto('/vendor_form');
 		await page.locator("[name=username]").fill("username");
 		await page.locator("[name=password]").fill("password");
-		await page.locator("[name=phone_no]").fill("0912345678");
+		await page.locator("[name=phone_number]").fill("0912345678");
 		await page.locator("[name=security_a]").fill("security answer");
 		await page.locator("[type=submit]").click();
-		await expect(page.getByRole('heading', { name: "Congratulations, you are now a vendor." })).toBeHidden();
+		await expect(page.getByText("Phone number must have format 0XXXXXXXXXX.")).toBeVisible();
 	});
 
 	test('has letters', async ({ page }) => {
 		await page.goto('/vendor_form');
 		await page.locator("[name=username]").fill("username");
 		await page.locator("[name=password]").fill("password");
-		await page.locator("[name=phone_no]").fill("09123456789a");
+		await page.locator("[name=phone_number]").fill("09123456789a");
 		await page.locator("[name=security_a]").fill("security answer");
 		await page.locator("[type=submit]").click();
-		await expect(page.getByRole('heading', { name: "Congratulations, you are now a vendor." })).toBeHidden();
+		await expect(page.getByText("Phone number must have format 0XXXXXXXXXX.")).toBeVisible();
 	});
 
 	test('does not start with 0', async ({ page }) => {
 		await page.goto('/vendor_form');
 		await page.locator("[name=username]").fill("username");
 		await page.locator("[name=password]").fill("password");
-		await page.locator("[name=phone_no]").fill("12345678900");
+		await page.locator("[name=phone_number]").fill("12345678900");
 		await page.locator("[name=security_a]").fill("security answer");
 		await page.locator("[type=submit]").click();
-		await expect(page.getByRole('heading', { name: "Congratulations, you are now a vendor." })).toBeHidden();
+		await expect(page.getByText("Phone number must have format 0XXXXXXXXXX.")).toBeVisible();
 	});
 });
