98b00c8971275a34452dbb80b14bd7cd19bdc65a
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-03-04 21:57:41 +0800

    feat: tweak skeleton to make it work good

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,29 +1,29 @@
+import { join } from 'path'
+import type { Config } from 'tailwindcss'
+import typography from '@tailwindcss/typography';
+import { skeleton } from '@skeletonlabs/tw-plugin'
 
-import { join } from 'path';
-import type { Config } from 'tailwindcss';
-
-// 1. Import the Skeleton plugin
-import { skeleton } from '@skeletonlabs/tw-plugin';
-
-const config = {
-	// 2. Opt for dark mode to be handled via the class method
+export default {
 	darkMode: 'class',
-	content: [
-		'./src/**/*.{html,js,svelte,ts}',
-		// 3. Append the path to the Skeleton package
-		join(require.resolve(
-			'@skeletonlabs/skeleton'),
-			'../**/*.{html,js,svelte,ts}'
-		)
-	],
+	content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
 	theme: {
 		extend: {},
 	},
 	plugins: [
-		// 4. Append the Skeleton plugin (after other plugins)
-		skeleton
-	]
+		typography,
+		skeleton({
+			themes: {
+				preset: [
+					{
+						name: 'skeleton',
+						enhancements: true,
+					},
+					{
+						name: 'rocket',
+						enhancements: true,
+					},
+				],
+			},
+		}),
+	],
 } satisfies Config;
-
-export default config;
-						
