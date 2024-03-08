0df13e112f1e7bcd38e586b5955102a2e1d09d87
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-03-08 15:21:30 +0800

    feat: add app header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,20 @@
 <script>
+	import { AppShell } from '@skeletonlabs/skeleton'
+	import { AppBar } from '@skeletonlabs/skeleton';
 	import '../app.pcss';
 </script>
 
 
-<div class="container h-full justify-left items-center mx-20 max-w-screen-lg prose dark:prose-invert text-justify mt-12">
-	<slot />
-</div>
+
+<AppShell>
+	<svelte:fragment slot="header"><AppBar background="bg-gradient-to-bl from-secondary-400 to-primary-400">UP Food Finder</AppBar></svelte:fragment>
+	<!-- (sidebarLeft) -->
+	<!-- (sidebarRight) -->
+	<!-- (pageHeader) -->
+	<div class="container h-full justify-left items-center mx-20 max-w-screen-lg prose dark:prose-invert text-justify mt-12">
+		<slot />
+	</div>
+	<!-- ---- / ---- -->
+	<!-- (pageFooter) -->
+	<!-- (footer) -->
+</AppShell>
