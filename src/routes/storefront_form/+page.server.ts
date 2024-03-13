f110884018a8c4d57f8e49989b309e5524cb78d0
Immanuel Josiah Balete <iabalete@up.edu.ph>
2024-03-13 18:03:02 +0800

    feat(page.server.ts): include coordinates in registerStorefront

--- src/routes/storefront_form/+page.server.ts
+++ src/routes/storefront_form/+page.server.ts
@@ -1,5 +1,5 @@
 import { fail } from '@sveltejs/kit';
-import { getStorefronts, registerStorefront, vendors } from '$lib/server/database';
+import { getStorefronts, registerStorefront, vendors, coordinates } from '$lib/server/database';
 
 // sample vendor as owner
 let vendor = vendors[0];
@@ -9,6 +9,7 @@ export const actions = {
     registerStorefront: async ({ request }: any) => {
         const formData: FormData = await request.formData();
         const storeName = String(formData.get("storename"));
+        const storeCoords : coordinates = [+formData.get("store_x"), +formData.get("store_y")];
         const owner = vendor;
         const menuItemCount = (Array.from(formData.keys()).length - NON_MENU) / 2; // remove non menu items then halve for name and price
         let menu = [];
@@ -27,6 +28,7 @@ export const actions = {
             storeName,
             owner,
             menu,
+            storeCoords,
         )
 
         console.log(getStorefronts());
