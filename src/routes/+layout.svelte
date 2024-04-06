34bfc4123462cbef4ddcc7a1c3b8d384ec84b969
Pau <mpgabcede@gmail.com>
2024-04-07 01:40:09 +0800

    feat(layout.svelte): add hyperlink to UP Food Finder text on AppBar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,7 +7,7 @@
 
 
 <AppShell>
-	<svelte:fragment slot="header"><AppBar background="bg-surface-50 text-primary-500"><h4 class="h4">UP Food Finder</h4></AppBar></svelte:fragment>
+	<svelte:fragment slot="header"><AppBar background="bg-surface-50 text-primary-500"><h4 class="h4"><a href="/">UP Food Finder</a></h4></AppBar></svelte:fragment>
 	<!-- (sidebarLeft) -->
 	<!-- (sidebarRight) -->
 	<!-- (pageHeader) -->
