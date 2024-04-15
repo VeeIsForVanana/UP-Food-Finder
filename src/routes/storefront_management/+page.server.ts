cd4890be0fa12640fbe8507fa90ac68be09ba5d9
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-04-15 15:41:14 +0800

    feat!(routes/storefront_management): implement changes to actions

--- src/routes/storefront_management/+page.server.ts
+++ src/routes/storefront_management/+page.server.ts
@@ -1,7 +1,8 @@
 import { fail } from '@sveltejs/kit';
-import { getStorefronts, updateStorefront, getVendorStorefronts, deleteStorefront, isStorefrontNameExists } from '$lib/server/database/storefronts';
-import { type MenuItem, Vendor } from '$lib/server/dataTransferObjects.js';
+import { getStorefronts, updateStorefront, getVendorStorefronts, deleteStorefront, isStorefrontNameExists, getStorefrontsFromNames } from '$lib/server/database/storefronts';
+import { type MenuItem, Storefront, Vendor } from '$lib/server/dataTransferObjects.js';
 import type { coordinates } from '$lib/constants';
+import { RequestEvent } from '../../../.svelte-kit/types/src/routes/vendor_form/$types';
 
 // sample vendor as owner
 const NON_MENU = 7; // number of fields in form not for menu
@@ -27,64 +28,68 @@ export const actions = {
     updateStorefront: async ({ request }) => {
         console.log("page.server.ts actions");
         const formData: FormData = await request.formData();
-        const index = Number(formData.get('selectedStorefrontIndex'));
-        const deleteStorefrontBoolean = formData.get('deleteStorefrontBoolean') === "true";
-        if (deleteStorefrontBoolean) {
-            console.log("DELETING STOREFRONT");
-            deleteStorefront(vendor, index);
-            return { storefrontDeleteSuccess: true }; 
-        }
+        const oldStorefrontName: string = formData.get('oldStorefrontName') as string;
+        const updatedStorefront: Storefront = formData.get('storefront') as unknown as Storefront;
 
-        const renameStorefront = String(formData.get('new_storename'));
-        let storeName;
-        if (renameStorefront) {
-            storeName = String(formData.get("new_storename"));
-        }
-        else {
-            storeName = String(formData.get("storename"));
-        }
+        // const index = Number(formData.get('selectedStorefrontIndex'));
+        // const deleteStorefrontBoolean = formData.get('deleteStorefrontBoolean') === "true";
+        // if (deleteStorefrontBoolean) {
+        //     console.log("DELETING STOREFRONT");
+        //     deleteStorefront(vendor, index);
+        //     return { storefrontDeleteSuccess: true }; 
+        // }
 
-        // Start of error checking
+        // const renameStorefront = String(formData.get('new_storename'));
+        // let storeName;
+        // if (renameStorefront) {
+        //     storeName = String(formData.get("new_storename"));
+        // }
+        // else {
+        //     storeName = String(formData.get("storename"));
+        // }
 
-        storeName.trim(); // remove leading and trailing whitespaces
-        const storefrontExists = await isStorefrontNameExists(storeName);
-        if (storefrontExists && renameStorefront) {
-            return fail(400, { storeNameExists: true });
-        } // check if any of the store name is taken
+        // // Start of error checking
 
-        // End of error checking
+        // storeName.trim(); // remove leading and trailing whitespaces
+        // const storefrontExists = await isStorefrontNameExists(storeName);
+        // if (storefrontExists && renameStorefront) {
+        //     return fail(400, { storeNameExists: true });
+        // } // check if any of the store name is taken
+
+        // // End of error checking
         
-        const owner = vendor;
-        const menuItemCount = (Array.from(formData.keys()).length - NON_MENU) / 2; // remove non menu items then halve for name and price
-        const coords: coordinates = [+formData.get("new_xcoords")!, +formData.get("new_ycoords")!];
+        // const owner = vendor;
+        // const menuItemCount = (Array.from(formData.keys()).length - NON_MENU) / 2; // remove non menu items then halve for name and price
+        // const coords: coordinates = [+formData.get("new_xcoords")!, +formData.get("new_ycoords")!];
         
-        const menu: MenuItem[] = [];
-        for (let i = 0; i < menuItemCount; i++) {
-            menu.push({
-                foodName: formData.get(`menu_name_${i}`)?.toString() ?? '',
-                price: +formData.get(`menu_price_${i}`)!,
-            });
+        // const menu: MenuItem[] = [];
+        // for (let i = 0; i < menuItemCount; i++) {
+        //     menu.push({
+        //         foodName: formData.get(`menu_name_${i}`)?.toString() ?? '',
+        //         price: +formData.get(`menu_price_${i}`)!,
+        //     });
+        // }
+        // console.log("MENU");
+        // console.log(menu);
+        
+        if (oldStorefrontName && updatedStorefront && !isStorefrontNameExists(updatedStorefront.getStoreName())) {
+            updateStorefront(
+                oldStorefrontName,
+                updatedStorefront
+            )
         }
-        console.log("MENU");
-        console.log(menu);
-
-        updateStorefront(
-            index,
-            storeName,
-            owner,
-            menu,
-            coords
-        )
-
-        console.log("Storefronts in database:");        
-        console.log(getStorefronts());
-        console.log("Storefronts of Vendor");
-        console.log(getVendorStorefronts(vendor));
 
         return { storefrontUpdateSuccess: true };
-    },
+    }, 
+
 
     deleteStorefront: async ({ request }) => {
-
+        const formData: FormData = await request.formData();
+        const oldStorefrontName: string = formData.get('oldStorefrontName') as string;
+        const targets: (Storefront | null)[] = await getStorefrontsFromNames([oldStorefrontName]) ?? [];
+        
+        if(targets[0]) {
+            deleteStorefront(targets[0])
+        }
     }
 }
