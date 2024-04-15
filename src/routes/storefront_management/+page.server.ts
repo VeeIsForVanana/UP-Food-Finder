360f679d01759a8f88b5135ed5737b93032fe66f
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-04-15 14:50:30 +0800

    feat!(routes/storefront_management): change how storefronts are loaded to the form

--- src/routes/storefront_management/+page.server.ts
+++ src/routes/storefront_management/+page.server.ts
@@ -1,37 +1,25 @@
 import { fail } from '@sveltejs/kit';
 import { getStorefronts, updateStorefront, getVendorStorefronts, deleteStorefront, isStorefrontNameExists } from '$lib/server/database/storefronts';
-import { vendors } from '$lib/server/database/vendors';
-import { type MenuItem } from '$lib/server/dataTransferObjects.js';
+import { type MenuItem, Vendor } from '$lib/server/dataTransferObjects.js';
 import type { coordinates } from '$lib/constants';
 
 // sample vendor as owner
-const vendor = vendors[0];
 const NON_MENU = 7; // number of fields in form not for menu
 
 /** @type {import('./$types').PageLoad} */
 export async function load() {
+    const vendor = new Vendor(
+        "upfoodfinder",
+        "password",
+        "01234567890",
+        "idk",
+        "doesn't matter",
+    );
+
     const storefronts = await getVendorStorefronts(vendor) ?? [];
 
-    console.log("page.server.ts load");
-    console.log(vendor);
-
-    console.log("Storefronts in Database");
-    console.log(await getStorefronts());
-
-    const storefrontsNames = storefronts.map(storefront => storefront.getStoreName());
-    console.log("Storefronts owned by Vendor");
-    console.log(storefrontsNames);
-    
-    const storefrontsMenuItems = storefronts.map(storefront => storefront.getMenu());
-    console.log("Menu items of storefronts owned by Vendor");
-    console.log(storefrontsMenuItems);
-
-    const storefrontsCoords = storefronts.map(storefront => storefront.getCoords());
-
     return {
-        storefrontsNames: storefrontsNames,
-        storefrontsMenuItems: storefrontsMenuItems,
-        storefrontsCoords: storefrontsCoords
+        storefronts
     };
 }
 
