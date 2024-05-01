8f0ddde8dad77d40621bed4ec2f181ce18c4dfcf
wtmaceda <wtmaceda@up.edu.ph>
2024-05-01 18:47:32 +0800

    maybe complete

--- src/routes/storefront_management/+page.server.ts
+++ src/routes/storefront_management/+page.server.ts
@@ -1,12 +1,12 @@
 import { getVendorStorefronts } from '$lib/server/database/storefronts';
 import { getLoggedInVendor } from '$lib/server/database/vendors';
-import { Storefront } from '$lib/server/dataTransferObjects.js';
+import { Vendor, Storefront } from '$lib/server/dataTransferObjects.js';
 
 /** @type {import('./$types').PageLoad} */
 export async function load({ locals: {supabase} }) {
     
-    const vendor = await getLoggedInVendor(supabase);
-
+    const vendor : Vendor = await getLoggedInVendor(supabase);
+    
     const rawStorefronts: (Storefront | null)[] = await getVendorStorefronts(vendor) ?? [];
     const storefronts = rawStorefronts
         .filter((storefront): storefront is Storefront => storefront !== null)
