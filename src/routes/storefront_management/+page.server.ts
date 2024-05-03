d81603a6317c808d1861b6aa3f89990c4ed22dbb
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-05-03 14:22:49 +0800

    feat: handle unauthorized access error better

--- src/routes/storefront_management/+page.server.ts
+++ src/routes/storefront_management/+page.server.ts
@@ -1,11 +1,15 @@
 import { getVendorStorefronts } from '$lib/server/database/storefronts';
 import { getLoggedInVendor } from '$lib/server/database/vendors';
 import { Vendor, Storefront } from '$lib/server/dataTransferObjects.js';
+import { error } from '@sveltejs/kit';
 
 /** @type {import('./$types').PageLoad} */
 export async function load({ locals: {supabase} }) {
     
     const vendor : Vendor = await getLoggedInVendor(supabase);
+    if (!vendor) {
+        error(401, {message: "You are not authorized to view this resource"})
+    }
     
     const rawStorefronts: (Storefront | null)[] = await getVendorStorefronts(vendor, supabase) ?? [];
     const storefronts = rawStorefronts
