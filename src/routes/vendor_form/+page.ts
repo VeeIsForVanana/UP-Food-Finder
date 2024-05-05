81198b53a77cbdb208a6de757c5e11085ee9f771
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-05-05 18:23:10 +0800

    fix: streamline error-checking on vendor form

--- src/routes/vendor_form/+page.ts
+++
@@ -1,9 +0,0 @@
-/** @type {import('./$types').PageLoad} */
-export async function load({ data }) {
-	// console.log("loading page data");
-	// console.log(`loaded ${getSecurityQuestions().length} security questions`)
-
-	return {
-		userVendor: data.userVendor,
-	};
-}
