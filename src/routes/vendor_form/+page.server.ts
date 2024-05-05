e980058838dcbb5efd804123ae3c031b93488d1b
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-05-05 17:55:08 +0800

    fix(vendor_form/+page.server.ts): change how the server handles vendor form errors

--- src/routes/vendor_form/+page.server.ts
+++ src/routes/vendor_form/+page.server.ts
@@ -17,8 +17,7 @@ export const actions = {
         const username = String(formData.get("username"));
         const phoneNumber = String(formData.get("phone_number"));
 
-        let failure = false;
-        const returnData = { };
+        const returnData = { status: 200, statusText: "Vendor successfully registered!" };
 
         const { session } = await safeGetSession()
 
@@ -28,41 +27,41 @@ export const actions = {
         
         // check if the user is logged in
         if (user === 'null') {
-            failure = true;
-            returnData.userError = true;
+            returnData.status = 401;
+            returnData.statusText = "";
         }
         
         // check if any of the necessary fields is missing and return an error if so
         [username, phoneNumber].forEach((elem) => {
             if (elem === 'null' || !elem) {
-                failure = true;
-                returnData.missing = true;
+                returnData.status = 422;
+                returnData.statusText = "A field is missing."
             }
         });
         
         // Check if username already exists in the database
         const usernameExists = await isUsernameExists(username, supabase);
         if (usernameExists) {
-            failure = true;
-            returnData.usernameExists = true;
+            returnData.status = 422;
+            returnData.statusText = "This username is already registered"
         }
 
         // Check if phone number already exists in the database
         const phoneNumberExists = await isPhoneNumberExists(phoneNumber, supabase);
         if (phoneNumberExists) {
-            failure = true;
-            returnData.phoneNumberExists = true;
+            returnData.status = 422;
+            returnData.statusText = "This phone number is already registered"
         }
         
         // perform additional check on inputs
         const phoneNumberRegex = new RegExp("^0[0-9]{10}$");
         if (!phoneNumberRegex.test(phoneNumber)) {
-            failure = true;
-            returnData.phoneError = true;
+            returnData.status = 422;
+            returnData.statusText = "This phone number is not in a valid format"
         }
         
-        if(failure && returnData != null) {
-            return fail(400, returnData);
+        if(returnData.status != 200) {
+            return fail(returnData.status, returnData);
         }
         
         // successful registration
@@ -73,6 +72,6 @@ export const actions = {
             supabase
         )
 
-        return { registrationSuccess: true };
+        return returnData;
     }
 }
