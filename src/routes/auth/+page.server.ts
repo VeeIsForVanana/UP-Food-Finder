8ec34aa69ef803dd29c2dbb6af1e692da10633ee
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-05-16 00:46:01 +0800

    feat: implement vendor guard for /auth route

--- src/routes/auth/+page.server.ts
+++ src/routes/auth/+page.server.ts
@@ -1,8 +1,10 @@
 import { getUserVendor } from '$lib/server/database/vendors';
-import type { PageServerLoad } from './$types';
+import { redirect } from '@sveltejs/kit';
 
 export async function load({ locals: { supabase } }) {
     const userVendor = ( await getUserVendor(supabase) )[0] ?? null;
+
+    if(!userVendor) redirect(303, 'vendor_form');
 
     return { userVendor }
 }
--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
-    import type { PageData } from './$types';
+	import type { PageData } from './$types';
     
     export let data: PageData;
 
     let { userVendor } = data;
+
+    console.log(userVendor)
 </script>
 
+<h3 class="h3">Welcome, {userVendor.username}</h3>
