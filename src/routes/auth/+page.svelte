3d855aa4f5dff382520aa510bcbc7ecb603cee02
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-05-16 00:58:31 +0800

    feat: add navigation to auth route

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -4,8 +4,13 @@
     export let data: PageData;
 
     let { userVendor } = data;
-
-    console.log(userVendor)
 </script>
 
 <h3 class="h3">Welcome, {userVendor.username}</h3>
+
+<h5 class="h5">Please use the links below to manage or create your storefronts:</h5>
+
+<div class="container p-10 flex-row">
+    <a href="storefront_form" class="block"><button class="btn-md">New Storefront</button></a>
+    <a href="storefront_management" class="block"><button class="btn-md">Storefront Management</button></a>
+</div>
