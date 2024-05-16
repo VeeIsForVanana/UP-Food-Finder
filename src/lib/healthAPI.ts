7a4054dd90fad912b9b97c9a6a71ce97fa52da39
Pau <mpgabcede@gmail.com>
2024-05-16 14:10:42 +0800

    feat(healthAPI): take care of unavailable foods

--- src/lib/healthAPI.ts
+++ src/lib/healthAPI.ts
@@ -5,12 +5,22 @@ export async function searchFood(foodName: string) {
     const url = `https://api.nal.usda.gov/fdc/v1/foods/search?api_key=${PUBLIC_USDA_KEY}&query=${foodName}&pageSize=1`;
     const response = await fetch(url);
     const data = await response.json();
-    return data.foods[0].fdcId;
+
+    return data.foods[0]?.fdcId;
 }
 
 export async function getNutrition(foodName: string) {
-    // returns json of health info
-    const id = await searchFood(foodName);  
+    // returns {Calories: string, Protein: string, Fat: string, Carbohydrate: string}
+    const id = await searchFood(foodName);
+    if (id === undefined) {
+        return {
+            Calories: 'not available',
+            Protein: 'not available',
+            Fat: 'not available',
+            Carbohydrate: 'not available'
+        };
+    }
+
     const url = `https://api.nal.usda.gov/fdc/v1/food/${id}?api_key=${PUBLIC_USDA_KEY}`;
     const response = await fetch(url);
     const data = await response.json();
